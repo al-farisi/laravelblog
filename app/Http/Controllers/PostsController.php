8c1012b6f429ebb87f6770e26f0d307c428c45c4
M Salman Al Farisi <MuhammadSalman.AlFarisi@mitrais.com>
2017-11-15 16:11:35 +0700

    Laracast e18

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,11 @@ use App\Post;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     public function index()
     {
         $posts = Post::latest()->get();
@@ -43,7 +48,12 @@ class PostsController extends Controller
             'body' => 'required|min:10'
         ]);
 
-        Post::create(request(['title', 'body']));
+        //Post::create([request(['title', 'body'])]);
+        Post::create([
+            'title'=>request('title'),
+            'body'=>request('body'),
+            'user_id'=>auth()->id
+        ]);
 
         return redirect('/');
     }
